868dc0714f353126d247d361007d608df56f3c45
p9kim <p9kim@ucsd.edu>
2016-02-05 18:04:32 +0000

    A5

--- static/js/app.js
+++ static/js/app.js
@@ -30,13 +30,6 @@ $('.navbar-collapse ul li a').click(function() {
   }
 });
 
-// Google Maps Scripts
-var map = null;
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-});
 
 function init() {
     // Basic options for a simple Google Map
@@ -167,10 +160,8 @@ function init() {
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
